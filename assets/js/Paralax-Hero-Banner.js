0f6cbcbdf0600137e1a14ff392131140df7ce6e2
Balasubramaniam A <63883008+Bala-1729@users.noreply.github.com>
2020-09-29 16:04:10 +0530

    Events view changed

--- assets/js/Paralax-Hero-Banner.js
+++ assets/js/Paralax-Hero-Banner.js
@@ -1,13 +1,12 @@
-
 var jumboHeight = $('.jumbotron').outerHeight();
-function parallax(){
+
+function parallax() {
     var scrolled = $(window).scrollTop();
     $('.jumbotron').css('background-position', 'center ' + -(scrolled * 0.2) + 'px');
-    console.log(jumboHeight-scrolled);
 }
 
-$(window).scroll(function(e){
-    
+$(window).scroll(function(e) {
+
     parallax();
-    
+
 });
